119a2b57a5fa86d4b3ac5aed52e31c1f79a6bba5
Maioon-png <mi_mkb.12_5@icloud.com>
2020-09-22 19:09:21 +0900

    Initial Commit

---
+++ Swift5GoogleAdmob/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5GoogleAdmob
+//
+//  Created by 岩崎舞 on 2020/09/22.
+//  Copyright © 2020 岩崎舞. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
