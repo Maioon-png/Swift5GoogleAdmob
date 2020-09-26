5ba1c83b20d1e206dc744b8a5279125d674034a0
Maioon-png <mi_mkb.12_5@icloud.com>
2020-09-26 11:51:05 +0900

    add delegate method

--- Swift5GoogleAdmob/TableViewController.swift
+++ Swift5GoogleAdmob/TableViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import GoogleMobileAds
 
-class TableViewController: UITableViewController {
+class TableViewController: UITableViewController,GADBannerViewDelegate,GADInterstitialDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
